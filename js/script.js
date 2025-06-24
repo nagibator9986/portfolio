f00efa513a548e0198d1a2c4f6b8b366abdd36f9
nagibator9986 <tleubekov.super@gmail.com>
2025-06-24 15:57:56 +0500

    cop

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            }
+        });
+    }, options);
+
+    sections.forEach(section => observer.observe(section));
+});
